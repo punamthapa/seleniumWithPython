48efe21d50045f6e7bc36bfa6fa378568c8fb640
punamthapa <punam.chhetri.372@gmail.com>
2023-08-07 22:48:20 +0545

    find element

--- Seleniumtest/test1.py
+++ Seleniumtest/test1.py
@@ -2,8 +2,11 @@
 from selenium import webdriver
 
 # create webdriver object
-driver = webdriver.Firefox()
-# driver =webdriver.Chrome()
-
+# driver = webdriver.Firefox()
+driver =webdriver.Chrome()
+driver.maximize_window()
 # get google.co.in
 driver.get("https://google.com")
+driver.find_element_by_name("q").send_keys("books")
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+driver.close()
