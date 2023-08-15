d8e8b0731d38dd7028973681476962dbca0a62b2
punamthapa <punam.chhetri.372@gmail.com>
2023-08-15 17:11:03 +0545

    parent xpath axes

--- Seleniumtest/xpathAxes.py
+++ Seleniumtest/xpathAxes.py
@@ -7,6 +7,11 @@ driver = webdriver.Chrome()
 driver.get("https://money.rediff.com/gainers/bse/daily/groupall")
 
 # self
-text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/self::a").text
+# text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/self::a").text
+# print(text_msg)
+# time.sleep(5)
+
+# parent : since parent of this selector is td
+text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/parent::td").text
 print(text_msg)
 time.sleep(5)
