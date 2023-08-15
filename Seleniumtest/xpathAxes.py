60ece70bf52f0908e01119461879b6108aa0a293
punamthapa <punam.chhetri.372@gmail.com>
2023-08-15 17:24:10 +0545

    child xpath axes

--- Seleniumtest/xpathAxes.py
+++ Seleniumtest/xpathAxes.py
@@ -12,6 +12,11 @@ driver.get("https://money.rediff.com/gainers/bse/daily/groupall")
 # time.sleep(5)
 
 # parent : since parent of this selector is td
-text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/parent::td").text
-print(text_msg)
+# text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/parent::td").text
+# print(text_msg)
+# time.sleep(5)
+
+# child : since td has no child, so we went to ancestor tr and get its child
+childs = driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/ancestor::tr/child::td")
+print(len(childs))
 time.sleep(5)
