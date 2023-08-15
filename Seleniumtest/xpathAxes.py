df14ee8b2f144fecbd2f4c1ccac7e99f561577f3
punamthapa <punam.chhetri.372@gmail.com>
2023-08-15 17:07:44 +0545

    self xpath axes

---
+++ Seleniumtest/xpathAxes.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://money.rediff.com/gainers/bse/daily/groupall")
+
+# self
+text_msg =driver.find_element(By.XPATH,"//a[contains(text(),'India Tourism De')]/self::a").text
+print(text_msg)
+time.sleep(5)
