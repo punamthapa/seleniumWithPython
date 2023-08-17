d620bbadd3cf7d8159b38fa0cb064319bac269c0
punamthapa <punam.chhetri.372@gmail.com>
2023-08-17 19:32:57 +0545

    checboxes

--- Seleniumtest/checkboxes.py
+++ Seleniumtest/checkboxes.py
@@ -33,7 +33,12 @@ checkboxes = driver.find_element(By.XPATH,"//input[@type='checkbox',and contains
 # for i in range(len(checkboxes)-2,len(checkboxes)):
 #     checkboxes[i].click()
 
-select first 2 checkboxes
-for i in range(len(checkboxes)):
-    if i<2:
-        checkboxes[i].click()
+#5) select first 2 checkboxes
+# for i in range(len(checkboxes)):
+#     if i<2:
+#         checkboxes[i].click()
+
+# 6) clearing all the check checkboxes
+# for checkbox in checkboxes:
+    # if checkbox.is_selected():
+    #     checkbox.click()
