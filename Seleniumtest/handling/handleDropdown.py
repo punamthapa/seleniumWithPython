b15c25033d797266ebc337914d7d110618eb175c
punamthapa <punam.chhetri.372@gmail.com>
2023-08-18 10:58:32 +0545

    dropdown

--- Seleniumtest/handling/handleDropdown.py
+++ Seleniumtest/handling/handleDropdown.py
@@ -11,6 +11,13 @@ driver.find_element(By.XPATH,"//a[@class='btn btn-black navbar-btn']").click()
 dropdownCountry=Select(driver.find_element(By.XPATH,"//select[@id='input-country']"))
 
 # select option
-dropdownCountry.select_by_visible_text("Nepal")
+# dropdownCountry.select_by_visible_text("Nepal")
 # dropdownCountry.Select_by_value("10")
 # dropdownCountry.Select_by_index(12)
+
+# capture all the options and print them
+alloptions =dropdownCountry.options
+print("total number of options",len(alloptions))
+
+for alloption in alloptions:
+    print(alloption.text)
