8d442a18a76a1be3a88dfb230d344f84adfbdbd3
punamthapa <punam.chhetri.372@gmail.com>
2023-08-18 10:15:21 +0545

    handle links

--- Seleniumtest/handlelinks.py
+++ Seleniumtest/handlelinks.py
@@ -6,4 +6,8 @@ driver.get("https://demo.nopcommerce.com")
 
 # click on link
 # driver.find_element(By.LINK_TEXT,"Digital downloads").click()
-driver.find_element(By.PARTIAL_LINK_TEXT,"Digital").click()
+# driver.find_element(By.PARTIAL_LINK_TEXT,"Digital").click()
+
+# find number of links in page
+links = driver.find_elements(By.TAG_NAME,'a')
+print("Total number of links:", len(links))
