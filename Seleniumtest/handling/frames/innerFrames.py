d926d75de667c2f9c563f8eb79f35eeaa364c649
punamthapa <punam.chhetri.372@gmail.com>
2023-08-23 17:18:40 +0545

    innerframes

--- Seleniumtest/handling/frames/innerFrames.py
+++ Seleniumtest/handling/frames/innerFrames.py
@@ -7,6 +7,15 @@ driver = webdriver.Chrome()
 
 driver.get("https://demo.automationtesting.in/Frames.html")
 driver.maximize_window()
+
 driver.find_element(By.XPATH,"//a[normalize-space()='Iframe with in an Iframe']").click()
+
 outerFrame= driver.find_element(By.XPATH,"//iframe[@src='MultipleFrames.html']")
 driver.switch_to.frame(outerFrame)
+
+innerFrame= driver.find_element(By.XPATH,"iframe[src='SingleFrame.html']")
+driver.switch_to.frame(innerFrame)
+
+driver.find_element(By.XPATH,"//input[@type='text']").send_keys("welcome")
+
+time.sleep(10)
