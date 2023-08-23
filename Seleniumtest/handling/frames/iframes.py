5ca3be443dc564966cb45714e6b906c932c902a7
punamthapa <punam.chhetri.372@gmail.com>
2023-08-23 17:07:48 +0545

    iframes

--- Seleniumtest/handling/frames/iframes.py
+++ Seleniumtest/handling/frames/iframes.py
@@ -8,6 +8,14 @@ driver = webdriver.Chrome()
 driver.get("https://www.selenium.dev/selenium/docs/api/java/overview-summary.html")
 driver.maximize_window()
 
+driver.switch_to.frame("packageListFrame")
 driver.find_element(By.LINK_TEXT,"org.openqa.selenium").click()
+driver.switch_to.default_content() #go back to main page
+
+driver.switch_to.frame("packageFrame")
 driver.find_element(By.LINK_TEXT,"WebDriver").click()
+driver.switch_to.default_content()
+
+driver.switch_to.frame("classFrame")
 driver.find_element(By.XPATH,"/html[1]/body[1]/header[1]/nav[1]/div[1]/div[1]/ul[1]/li[8]").click()
+time.sleep(5)
