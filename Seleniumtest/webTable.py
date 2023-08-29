f9ac6a90b95fb861a7a839b57168e2b409e08db5
punamthapa <punam.chhetri.372@gmail.com>
2023-08-29 20:37:03 +0545

    table

--- Seleniumtest/webTable.py
+++ Seleniumtest/webTable.py
@@ -10,8 +10,13 @@ driver.maximize_window()
 
 
 # count the number of rows and columns
-noOfRows=len(driver.find_element(By.XPATH,"//table[@name='BookTable']//tr"))
-print(noOfRows)
+# noOfRows=len(driver.find_element(By.XPATH,"//table[@name='BookTable']//tr"))
+# print(noOfRows)
+#
+# noOfColumns=len(driver.find_element(By.XPATH,"//table[@name='BookTable']//tr[1]/th"))
+# print(noOfColumns)
 
-noOfColumns=len(driver.find_element(By.XPATH,"//table[@name='BookTable']//tr[1]/th"))
-print(noOfColumns)
+
+#read specific rows and column data
+data =driver.find_element(By.XPATH,"//table[@name='BookTable']/tbody/tr[5]/td[1]").text
+print(data)
